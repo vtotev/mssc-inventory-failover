ac44850f4235768688dc6836859c91d272d98cbe
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2024-02-25 17:18:55 +0200

    initial project create

---
+++ src/test/java/guru/sfg/brewery/inventoryfailover/MsscInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.inventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
